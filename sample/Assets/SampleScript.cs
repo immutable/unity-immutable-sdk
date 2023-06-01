3aa94f131f20c23b0855ebfd2440894930f2a9cd
Natalie Bunduwongse <nf1993@gmail.com>
2023-06-02 10:45:24 +1200

    fix: change from showing output in dialog to text on scene

--- sample/Assets/SampleScript.cs
+++ sample/Assets/SampleScript.cs
@@ -9,6 +9,8 @@ public class SampleScript : MonoBehaviour
 {
     [SerializeField] private Passport passport;
 
+    [SerializeField] private Text output;
+
     [SerializeField] private Button connectButton;
     [SerializeField] private Button getAddressButton;
     [SerializeField] private Button logoutButton;
@@ -20,20 +22,20 @@ public class SampleScript : MonoBehaviour
 
     public async void Connect() {
         bool success = await passport.Connect();
-        showDialog("Successfully connected to Passport");
+        showOutput("Successfully connected to Passport");
     }
 
     public async void GetAddress() {
         string? address = await passport.GetAddress();
-        showDialog($"Address {address}");
+        showOutput(address);
     }
 
     public async void Logout() {
         passport.Logout();
-        showDialog("Logged out");
+        showOutput("Logged out");
     }
 
-    private void showDialog(string message) {
-        EditorUtility.DisplayDialog("Output", message, "OK");
+    private void showOutput(string message) {
+        output.text = message;
     }
 }
