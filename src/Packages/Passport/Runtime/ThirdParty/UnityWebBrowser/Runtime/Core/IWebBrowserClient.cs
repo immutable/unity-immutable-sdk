f660bb63ad7537650418153472f769a6bab1841a
Dom Murray <dom-murray@users.noreply.github.com>
2023-06-20 13:40:32 +1000

    refactor: separated mapping out of browsercomms

---
+++ src/Packages/Passport/Runtime/ThirdParty/UnityWebBrowser/Runtime/Core/IWebBrowserClient.cs
@@ -0,0 +1,9 @@
+using VoltstroStudios.UnityWebBrowser.Events;
+
+namespace VoltstroStudios.UnityWebBrowser.Core {
+    public interface IWebBrowserClient {
+        event OnUnityPostMessageDelegate OnUnityPostMessage;
+
+        void ExecuteJs(string js);
+    }
+}
