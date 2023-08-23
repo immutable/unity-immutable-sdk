8692d27968478b46c774016194e0400a3b3d829b
Cassius Pacheco <cassius.pacheco@immutable.com>
2023-08-24 09:10:51 +1000

    feat: add NFT transfer option to sample app

--- sample/Assets/Scripts/AuthenticatedScript.cs
+++ sample/Assets/Scripts/AuthenticatedScript.cs
@@ -10,12 +10,21 @@ public class AuthenticatedScript : MonoBehaviour
 #pragma warning disable CS8618
     [SerializeField] private Text output;
 
+    [SerializeField] private Canvas authenticatedCanvas;
     [SerializeField] private Button accessTokenButton;
     [SerializeField] private Button idTokenButton;
     [SerializeField] private Button getAddressButton;
     [SerializeField] private Button logoutButton;
     [SerializeField] private Button signMessageButton;
+    [SerializeField] private Button showTransferButton;
     [SerializeField] private InputField signInput;
+    
+    [SerializeField] private Canvas transferCanvas;
+    [SerializeField] private InputField tokenIdInput;
+    [SerializeField] private InputField tokenAddressInput;
+    [SerializeField] private InputField receiverInput;
+    [SerializeField] private Button transferButton;
+    [SerializeField] private Button cancelTransferButton;
 
     private Passport passport;
 #pragma warning restore CS8618
@@ -72,6 +81,51 @@ public class AuthenticatedScript : MonoBehaviour
     {
         string? email = await passport.GetEmail();
         ShowOutput(email ?? "No email");
+    }
+
+    public async void ShowTransfer()
+    {
+        authenticatedCanvas.gameObject.SetActive(false);
+        transferCanvas.gameObject.SetActive(true);
+    }
+
+    public async void CancelTransfer()
+    {
+        authenticatedCanvas.gameObject.SetActive(true);
+        transferCanvas.gameObject.SetActive(false);
+        tokenIdInput.text = "";
+        tokenAddressInput.text = "";
+        receiverInput.text = "";
+    }
+
+    public async void Transfer()
+    {
+        if (tokenIdInput.text != "" && tokenAddressInput.text != "" && receiverInput.text != "")
+        {
+            transferButton.gameObject.SetActive(false);
+            cancelTransferButton.gameObject.SetActive(false);
+
+            try
+            {
+                UnsignedTransferRequest request = UnsignedTransferRequest.ERC721(
+                    receiverInput.text, 
+                    tokenIdInput.text, 
+                    tokenAddressInput.text
+                    );
+                CreateTransferResponseV1 response = await passport.ImxTransfer(request);
+                ShowOutput($"Transferred successfully. Transfer id: {response.TransferId}");
+                tokenIdInput.text = "";
+                tokenAddressInput.text = "";
+                receiverInput.text = "";
+            }
+            catch (Exception e)
+            {
+                ShowOutput($"Unable to transfer: {e.Message}");
+            }
+
+            transferButton.gameObject.SetActive(true);
+            cancelTransferButton.gameObject.SetActive(true);
+        }
     }
 
     private void ShowOutput(string message)
