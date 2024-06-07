1e00ef5234efc540ef38b540fe6e1701b1c8ce1e
Nik Ho <studying.mole@gmail.com>
2024-06-07 13:46:10 +1200

    test: username case

--- .github/scripts/check_team_membership.sh
+++ .github/scripts/check_team_membership.sh
@@ -5,7 +5,7 @@ set -x
 USER=$1
 TOKEN=$2
 
-response=$(curl -L -H "Authorization: Bearer $TOKEN" -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" "https://api.github.com/orgs/immutable/teams/sdk/memberships/$USER")
+response=$(curl -L -H "Authorization: Bearer $TOKEN" -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" "https://api.github.com/orgs/immutable/teams/sdk/memberships/codeschwert")
 
 echo "$response"
 
