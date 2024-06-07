709dd1a88418212aa6074d54cab29ab4dc5c3665
Nik Ho <studying.mole@gmail.com>
2024-06-07 13:15:18 +1200

    test: check membership script

--- .github/scripts/check_team_membership.sh
+++ .github/scripts/check_team_membership.sh
@@ -5,6 +5,8 @@ TOKEN=$2
 
 response=$(curl -s -H "Authorization: token $TOKEN" "https://api.github.com/orgs/immutable/teams/sdk/memberships/$USER")
 
+echo "$response"
+
 if echo "$response" | grep -q '"state": "active"'; then
   echo "true"
 else
