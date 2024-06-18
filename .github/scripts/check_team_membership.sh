4380865b991df0fa57b610528ce22c43eae03e8d
Nik Ho <studying.mole@gmail.com>
2024-06-18 23:13:33 +1200

    fix: linting

--- .github/scripts/check_team_membership.sh
+++ .github/scripts/check_team_membership.sh
@@ -3,17 +3,11 @@
 set -x
 
 USER=$1
-TOKEN=$2
-
-echo "$(gh api \
-  -H "Accept: application/vnd.github+json" \
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-  /orgs/immutable/teams/sdk/members)"
 
 response=$(gh api \
   -H "Accept: application/vnd.github+json" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  /orgs/immutable/teams/sdk/memberships/codeschwert)
+  /orgs/immutable/teams/sdk/memberships/$USER)
 
 echo "$response"
 
@@ -25,4 +19,4 @@ fi
 echo "$IS_MEMBER"
 
 # Set the environment variable for the GitHub workflow
-echo "IS_MEMBER=$IS_MEMBER" >> $GITHUB_ENV
+echo "IS_MEMBER=$IS_MEMBER" >> "$GITHUB_ENV"
