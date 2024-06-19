65721e52ca970921500af99caab007f9ae7b1715
Nik <2661899+CodeSchwert@users.noreply.github.com>
2024-06-19 17:35:33 +1200

    fix: update version workflow (#214)
    * fix: github token and check membership script

--- .github/scripts/check_team_membership.sh
+++ .github/scripts/check_team_membership.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
+set -x
+
 USER=$1
-TOKEN=$2
 
-response=$(curl -s -H "Authorization: token $TOKEN" "https://api.github.com/orgs/immutable/teams/sdk/memberships/$USER")
+response=$(gh api \
+  -H "Accept: application/vnd.github+json" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  "/orgs/immutable/teams/sdk/memberships/${USER}")
 
-if echo "$response" | grep -q '"state": "active"'; then
-  echo "true"
+echo "$response"
+
+if echo "$response" | grep -q '"state":"active"'; then
+  IS_MEMBER=true
 else
-  echo "false"
+  IS_MEMBER=false
 fi
+echo "$IS_MEMBER"
+
+# Set the environment variable for the GitHub workflow
+echo "IS_MEMBER=$IS_MEMBER" >> "$GITHUB_ENV"
