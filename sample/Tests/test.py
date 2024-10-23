cc83d1d4b787b00aae7f62766fa781a25e8f3f4d
Andrew Yang <ddvkid@gmail.com>
2024-10-23 12:31:32 +1100

    chore: add test file

---
+++ sample/Tests/test.py
@@ -0,0 +1,17 @@
+import unittest
+from alttester import *
+
+class UnityTest(unittest.TestCase):
+
+#     altdriver = None
+# 
+#     @classmethod
+#     def setUpClass(cls):
+#         cls.altdriver = AltDriver()
+# 
+#     @classmethod
+#     def tearDownClass(cls):
+#         cls.altdriver.stop()
+
+    def test(self):
+        assert True
