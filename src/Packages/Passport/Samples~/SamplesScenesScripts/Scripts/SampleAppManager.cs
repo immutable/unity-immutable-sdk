e356b73c274c9da6d6b15e39fc35193dccdfad47
Create or Update Pull Request Action <create-or-update-pull-request@users.noreply.github.com>
2024-10-25 03:30:25 +0000

    chore: update version

---
+++ src/Packages/Passport/Samples~/SamplesScenesScripts/Scripts/SampleAppManager.cs
@@ -0,0 +1,22 @@
+public static class SampleAppManager
+{
+    /// <summary>
+    /// Indicates whether the running platform supports PKCE.
+    /// </summary>
+    public static bool SupportsPKCE { get; set; }
+
+    /// <summary>
+    /// Indicates whether the selected authentication method is PKCE.
+    /// </summary>
+    public static bool UsePKCE { get; set; }
+
+    /// <summary>
+    /// Indicates whether the user is connected to IMX.
+    /// </summary>
+    public static bool IsConnectedToImx { get; set; }
+
+    /// <summary>
+    /// Indicates whether the user is connected to zkEVM.
+    /// </summary>
+    public static bool IsConnectedToZkEvm { get; set; }
+}
