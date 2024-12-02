0a3fb9944f388539197821fb219bcdf4a7c2c643
Natalie Bunduwongse <nf1993@gmail.com>
2024-12-02 14:01:33 +1300

    chore: hide launch browser button if using custom windows webview

--- sample/Assets/Scripts/AuthenticatedScript.cs
+++ sample/Assets/Scripts/AuthenticatedScript.cs
@@ -51,7 +51,7 @@ public class AuthenticatedScript : MonoBehaviour
             ShowOutput("Passport instance is null");
         }
         
-#if UNITY_STANDALONE_WIN || (UNITY_ANDROID && UNITY_EDITOR_WIN) || (UNITY_IPHONE && UNITY_EDITOR_WIN)
+#if !IMMUTABLE_CUSTOM_BROWSER && (UNITY_STANDALONE_WIN || (UNITY_ANDROID && UNITY_EDITOR_WIN) || (UNITY_IPHONE && UNITY_EDITOR_WIN))
         LaunchBrowserButton.gameObject.SetActive(true);
 #else
         LaunchBrowserButton.gameObject.SetActive(false);
