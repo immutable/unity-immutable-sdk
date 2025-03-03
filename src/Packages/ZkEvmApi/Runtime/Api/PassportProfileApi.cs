2b0b66a827a9210e6c8527f68d636165b860f4d3
GitHub Action <action@github.com>
2025-03-03 10:08:14 +0000

    feat: update immutable zkEVM API package

--- src/Packages/ZkEvmApi/Runtime/Api/PassportProfileApi.cs
+++ src/Packages/ZkEvmApi/Runtime/Api/PassportProfileApi.cs
@@ -67,6 +67,27 @@ namespace Immutable.Api.ZkEvm.Api
         /// <param name="linkWalletV2Request"> (optional)</param>
         /// <returns>ApiResponse of Wallet</returns>
         ApiResponse<Wallet> LinkWalletV2WithHttpInfo(LinkWalletV2Request? linkWalletV2Request = default(LinkWalletV2Request?));
+        /// <summary>
+        /// Send phone OTP code for user supplied phone number
+        /// </summary>
+        /// <remarks>
+        /// Send phone OTP code for user supplied phone number
+        /// </remarks>
+        /// <exception cref="Immutable.Api.ZkEvm.Client.ApiException">Thrown when fails to make API call</exception>
+        /// <param name="phoneNumberOTPRequest"> (optional)</param>
+        /// <returns></returns>
+        void SendPhoneOtp(PhoneNumberOTPRequest? phoneNumberOTPRequest = default(PhoneNumberOTPRequest?));
+
+        /// <summary>
+        /// Send phone OTP code for user supplied phone number
+        /// </summary>
+        /// <remarks>
+        /// Send phone OTP code for user supplied phone number
+        /// </remarks>
+        /// <exception cref="Immutable.Api.ZkEvm.Client.ApiException">Thrown when fails to make API call</exception>
+        /// <param name="phoneNumberOTPRequest"> (optional)</param>
+        /// <returns>ApiResponse of Object(void)</returns>
+        ApiResponse<Object> SendPhoneOtpWithHttpInfo(PhoneNumberOTPRequest? phoneNumberOTPRequest = default(PhoneNumberOTPRequest?));
         #endregion Synchronous Operations
     }
 
@@ -120,6 +141,29 @@ namespace Immutable.Api.ZkEvm.Api
         /// <param name="cancellationToken">Cancellation Token to cancel the request.</param>
         /// <returns>Task of ApiResponse (Wallet)</returns>
         System.Threading.Tasks.Task<ApiResponse<Wallet>> LinkWalletV2WithHttpInfoAsync(LinkWalletV2Request? linkWalletV2Request = default(LinkWalletV2Request?), System.Threading.CancellationToken cancellationToken = default(global::System.Threading.CancellationToken));
+        /// <summary>
+        /// Send phone OTP code for user supplied phone number
+        /// </summary>
+        /// <remarks>
+        /// Send phone OTP code for user supplied phone number
+        /// </remarks>
+        /// <exception cref="Immutable.Api.ZkEvm.Client.ApiException">Thrown when fails to make API call</exception>
+        /// <param name="phoneNumberOTPRequest"> (optional)</param>
+        /// <param name="cancellationToken">Cancellation Token to cancel the request.</param>
+        /// <returns>Task of void</returns>
+        System.Threading.Tasks.Task SendPhoneOtpAsync(PhoneNumberOTPRequest? phoneNumberOTPRequest = default(PhoneNumberOTPRequest?), System.Threading.CancellationToken cancellationToken = default(global::System.Threading.CancellationToken));
+
+        /// <summary>
+        /// Send phone OTP code for user supplied phone number
+        /// </summary>
+        /// <remarks>
+        /// Send phone OTP code for user supplied phone number
+        /// </remarks>
+        /// <exception cref="Immutable.Api.ZkEvm.Client.ApiException">Thrown when fails to make API call</exception>
+        /// <param name="phoneNumberOTPRequest"> (optional)</param>
+        /// <param name="cancellationToken">Cancellation Token to cancel the request.</param>
+        /// <returns>Task of ApiResponse</returns>
+        System.Threading.Tasks.Task<ApiResponse<Object>> SendPhoneOtpWithHttpInfoAsync(PhoneNumberOTPRequest? phoneNumberOTPRequest = default(PhoneNumberOTPRequest?), System.Threading.CancellationToken cancellationToken = default(global::System.Threading.CancellationToken));
         #endregion Asynchronous Operations
     }
 
@@ -514,6 +558,136 @@ namespace Immutable.Api.ZkEvm.Api
             if (this.ExceptionFactory != null)
             {
                 Exception _exception = this.ExceptionFactory("LinkWalletV2", localVarResponse);
+                if (_exception != null) throw _exception;
+            }
+
+            return localVarResponse;
+        }
+
+        /// <summary>
+        /// Send phone OTP code for user supplied phone number Send phone OTP code for user supplied phone number
+        /// </summary>
+        /// <exception cref="Immutable.Api.ZkEvm.Client.ApiException">Thrown when fails to make API call</exception>
+        /// <param name="phoneNumberOTPRequest"> (optional)</param>
+        /// <returns></returns>
+        public void SendPhoneOtp(PhoneNumberOTPRequest? phoneNumberOTPRequest = default(PhoneNumberOTPRequest?))
+        {
+            SendPhoneOtpWithHttpInfo(phoneNumberOTPRequest);
+        }
+
+        /// <summary>
+        /// Send phone OTP code for user supplied phone number Send phone OTP code for user supplied phone number
+        /// </summary>
+        /// <exception cref="Immutable.Api.ZkEvm.Client.ApiException">Thrown when fails to make API call</exception>
+        /// <param name="phoneNumberOTPRequest"> (optional)</param>
+        /// <returns>ApiResponse of Object(void)</returns>
+        public Immutable.Api.ZkEvm.Client.ApiResponse<Object> SendPhoneOtpWithHttpInfo(PhoneNumberOTPRequest? phoneNumberOTPRequest = default(PhoneNumberOTPRequest?))
+        {
+            Immutable.Api.ZkEvm.Client.RequestOptions localVarRequestOptions = new Immutable.Api.ZkEvm.Client.RequestOptions();
+
+            string[] _contentTypes = new string[] {
+                "application/json"
+            };
+
+            // to determine the Accept header
+            string[] _accepts = new string[] {
+                "application/json"
+            };
+
+            var localVarContentType = Immutable.Api.ZkEvm.Client.ClientUtils.SelectHeaderContentType(_contentTypes);
+            if (localVarContentType != null) localVarRequestOptions.HeaderParameters.Add("Content-Type", localVarContentType);
+
+            var localVarAccept = Immutable.Api.ZkEvm.Client.ClientUtils.SelectHeaderAccept(_accepts);
+            if (localVarAccept != null) localVarRequestOptions.HeaderParameters.Add("Accept", localVarAccept);
+
+            localVarRequestOptions.Data = phoneNumberOTPRequest;
+
+            // authentication (BearerAuth) required
+            // bearer authentication required
+            if (!string.IsNullOrEmpty(this.Configuration.AccessToken) && !localVarRequestOptions.HeaderParameters.ContainsKey("Authorization"))
+            {
+                localVarRequestOptions.HeaderParameters.Add("Authorization", "Bearer " + this.Configuration.AccessToken);
+            }
+
+            // make the HTTP request
+            var localVarResponse = this.Client.Post<Object>("/passport-profile/v1/phone-otp", localVarRequestOptions, this.Configuration);
+
+            if (this.ExceptionFactory != null)
+            {
+                Exception _exception = this.ExceptionFactory("SendPhoneOtp", localVarResponse);
+                if (_exception != null) throw _exception;
+            }
+
+            return localVarResponse;
+        }
+
+        /// <summary>
+        /// Send phone OTP code for user supplied phone number Send phone OTP code for user supplied phone number
+        /// </summary>
+        /// <exception cref="Immutable.Api.ZkEvm.Client.ApiException">Thrown when fails to make API call</exception>
+        /// <param name="phoneNumberOTPRequest"> (optional)</param>
+        /// <param name="cancellationToken">Cancellation Token to cancel the request.</param>
+        /// <returns>Task of void</returns>
+        public async System.Threading.Tasks.Task SendPhoneOtpAsync(PhoneNumberOTPRequest? phoneNumberOTPRequest = default(PhoneNumberOTPRequest?), System.Threading.CancellationToken cancellationToken = default(global::System.Threading.CancellationToken))
+        {
+            var task = SendPhoneOtpWithHttpInfoAsync(phoneNumberOTPRequest, cancellationToken);
+#if UNITY_EDITOR || !UNITY_WEBGL
+            await task.ConfigureAwait(false);
+#else
+            await task;
+#endif
+        }
+
+        /// <summary>
+        /// Send phone OTP code for user supplied phone number Send phone OTP code for user supplied phone number
+        /// </summary>
+        /// <exception cref="Immutable.Api.ZkEvm.Client.ApiException">Thrown when fails to make API call</exception>
+        /// <param name="phoneNumberOTPRequest"> (optional)</param>
+        /// <param name="cancellationToken">Cancellation Token to cancel the request.</param>
+        /// <returns>Task of ApiResponse</returns>
+        public async System.Threading.Tasks.Task<Immutable.Api.ZkEvm.Client.ApiResponse<Object>> SendPhoneOtpWithHttpInfoAsync(PhoneNumberOTPRequest? phoneNumberOTPRequest = default(PhoneNumberOTPRequest?), System.Threading.CancellationToken cancellationToken = default(global::System.Threading.CancellationToken))
+        {
+
+            Immutable.Api.ZkEvm.Client.RequestOptions localVarRequestOptions = new Immutable.Api.ZkEvm.Client.RequestOptions();
+
+            string[] _contentTypes = new string[] {
+                "application/json"
+            };
+
+            // to determine the Accept header
+            string[] _accepts = new string[] {
+                "application/json"
+            };
+
+
+            var localVarContentType = Immutable.Api.ZkEvm.Client.ClientUtils.SelectHeaderContentType(_contentTypes);
+            if (localVarContentType != null) localVarRequestOptions.HeaderParameters.Add("Content-Type", localVarContentType);
+
+            var localVarAccept = Immutable.Api.ZkEvm.Client.ClientUtils.SelectHeaderAccept(_accepts);
+            if (localVarAccept != null) localVarRequestOptions.HeaderParameters.Add("Accept", localVarAccept);
+
+            localVarRequestOptions.Data = phoneNumberOTPRequest;
+
+            // authentication (BearerAuth) required
+            // bearer authentication required
+            if (!string.IsNullOrEmpty(this.Configuration.AccessToken) && !localVarRequestOptions.HeaderParameters.ContainsKey("Authorization"))
+            {
+                localVarRequestOptions.HeaderParameters.Add("Authorization", "Bearer " + this.Configuration.AccessToken);
+            }
+
+            // make the HTTP request
+
+            var task = this.AsynchronousClient.PostAsync<Object>("/passport-profile/v1/phone-otp", localVarRequestOptions, this.Configuration, cancellationToken);
+
+#if UNITY_EDITOR || !UNITY_WEBGL
+            var localVarResponse = await task.ConfigureAwait(false);
+#else
+            var localVarResponse = await task;
+#endif
+
+            if (this.ExceptionFactory != null)
+            {
+                Exception _exception = this.ExceptionFactory("SendPhoneOtp", localVarResponse);
                 if (_exception != null) throw _exception;
             }
 
