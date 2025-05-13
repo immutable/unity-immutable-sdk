db7f60f3f700d9307d52be2c44648add9877dc22
Natalie Bunduwongse <nf1993@gmail.com>
2025-05-13 18:40:03 +1200

    refactor: move linking wallet example to zkevm api directory

---
+++ sample/Assets/Scripts/ZkEvmApi/ZkEvmApiScript.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ZkEvmApiScript : MonoBehaviour
+{
+    public void LinkWallet()
+    {
+        SceneManager.LoadScene("LinkWallet");
+    }
+}
