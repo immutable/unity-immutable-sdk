0725d8fdbcb26fd82fbb573cb567796128d54ad8
Create or Update Pull Request Action <create-or-update-pull-request@users.noreply.github.com>
2025-05-27 23:13:46 +0000

    chore: update version

---
+++ src/Packages/Passport/Samples~/SamplesScenesScripts/Scripts/Passport/AuthenticatedSceneManager.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class AuthenticatedSceneManager : MonoBehaviour
+{
+    [Header("IMX Buttons")]
+    [SerializeField] private Button ConnectButton;
+    [SerializeField] private Button IsRegisteredOffchainButton;
+    [SerializeField] private Button RegisterOffchainButton;
+    [SerializeField] private Button GetAddressButton;
+    [SerializeField] private Button NftTransferButton;
+
+    [Header("zkEVM Buttons")]
+    [SerializeField] private Button ConnectEvmButton;
+    [SerializeField] private Button SendTransactionButton;
+    [SerializeField] private Button RequestAccountsButton;
+    [SerializeField] private Button GetBalanceButton;
+    [SerializeField] private Button GetTransactionReceiptButton;
+    [SerializeField] private Button SignTypedDataButton;
+
+    public Action OnImxConnected;
+
+    void Awake()
+    {
+        OnImxConnected = () => { UpdateImxButtonStates(); };
+    }
+
+    void Start()
+    {
+        UpdateImxButtonStates();
+        UpdateZkEvmButtonStates();
+    }
+
+    public void UpdateImxButtonStates()
+    {
+        bool isConnected = SampleAppManager.IsConnectedToImx;
+        if (ConnectButton != null) ConnectButton.gameObject.SetActive(!isConnected);
+        if (IsRegisteredOffchainButton != null) IsRegisteredOffchainButton.gameObject.SetActive(isConnected);
+        if (RegisterOffchainButton != null) RegisterOffchainButton.gameObject.SetActive(isConnected);
+        if (GetAddressButton != null) GetAddressButton.gameObject.SetActive(isConnected);
+        if (NftTransferButton != null) NftTransferButton.gameObject.SetActive(isConnected);
+    }
+
+    public void UpdateZkEvmButtonStates()
+    {
+        bool isConnected = SampleAppManager.IsConnectedToZkEvm;
+        if (ConnectEvmButton != null) ConnectEvmButton.gameObject.SetActive(!isConnected);
+        if (SendTransactionButton != null) SendTransactionButton.gameObject.SetActive(isConnected);
+        if (RequestAccountsButton != null) RequestAccountsButton.gameObject.SetActive(isConnected);
+        if (GetBalanceButton != null) GetBalanceButton.gameObject.SetActive(isConnected);
+        if (GetTransactionReceiptButton != null) GetTransactionReceiptButton.gameObject.SetActive(isConnected);
+        if (SignTypedDataButton != null) SignTypedDataButton.gameObject.SetActive(isConnected);
+    }
+
+    public static void NavigateToAuthenticatedScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("AuthenticatedScene");
+    }
+
+    public static void NavigateToUnauthenticatedScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("UnauthenticatedScene");
+    }
+} 
---
+++ src/Packages/Passport/Samples~/SamplesScenesScripts/Scripts/Passport/UnauthenticatedSceneManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.UI;
+using Cysharp.Threading.Tasks;
+using Immutable.Passport;
+using System;
+
+public class UnauthenticatedSceneManager : MonoBehaviour
+{
+    [SerializeField] private GameObject LoginButtons;
+    [SerializeField] private GameObject ReloginButtons;
+    [SerializeField] private InputField DeviceCodeTimeoutMs;
+
+    public Action OnImxConnected;
+
+    private async void Start()
+    {
+        if (Passport.Instance != null)
+        {
+            bool hasCredsSaved = await Passport.Instance.HasCredentialsSaved();
+            ReloginButtons.SetActive(hasCredsSaved);
+            LoginButtons.SetActive(!hasCredsSaved);
+            DeviceCodeTimeoutMs.gameObject.SetActive(!hasCredsSaved && !SampleAppManager.UsePKCE);
+        }
+        else
+        {
+            Debug.LogError("[UnauthenticatedSceneManager] Passport.Instance is null");
+        }
+    }
+
+    void Awake()
+    {
+        OnImxConnected = () => { UnityEngine.SceneManagement.SceneManager.LoadScene("AuthenticatedScene"); };
+    }
+} 
