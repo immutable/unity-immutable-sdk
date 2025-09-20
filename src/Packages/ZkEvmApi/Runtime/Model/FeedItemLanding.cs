b8ddb5df5b691550fedce09aa75e11c52b5ae5f1
GitHub Action <action@github.com>
2025-09-20 10:07:40 +0000

    feat: update immutable zkEVM API package

--- src/Packages/ZkEvmApi/Runtime/Model/FeedItemLanding.cs
+++ src/Packages/ZkEvmApi/Runtime/Model/FeedItemLanding.cs
@@ -71,10 +71,11 @@ namespace Immutable.Api.ZkEvm.Model
         /// <param name="gameName">Game name.</param>
         /// <param name="questCompletedPopupText">Text to display when the quest is completed in an onboarding experience.</param>
         /// <param name="tags">The tags for the feed item.</param>
+        /// <param name="controlTags">Some specific tags for controlling feed items.</param>
         /// <param name="categories">The categories for the feed item.</param>
         /// <param name="onboardingExperience">The onboarding experience for the feed item.</param>
         /// <param name="type">Feed item type (required).</param>
-        public FeedItemLanding(string id = default(string), string name = default(string), string questId = default(string), int priority = default(int), int gemsEarnable = default(int), bool bypass = default(bool), bool dayZero = default(bool), Guid gameId = default(Guid), string gameName = default(string), string questCompletedPopupText = default(string), List<string> tags = default(List<string>), List<string> categories = default(List<string>), string onboardingExperience = default(string), TypeEnum type = default(TypeEnum))
+        public FeedItemLanding(string id = default(string), string name = default(string), string questId = default(string), int priority = default(int), int gemsEarnable = default(int), bool bypass = default(bool), bool dayZero = default(bool), Guid gameId = default(Guid), string gameName = default(string), string questCompletedPopupText = default(string), List<string> tags = default(List<string>), List<string> controlTags = default(List<string>), List<string> categories = default(List<string>), string onboardingExperience = default(string), TypeEnum type = default(TypeEnum))
         {
             // to ensure "id" is required (not null)
             if (id == null)
@@ -103,6 +104,7 @@ namespace Immutable.Api.ZkEvm.Model
             this.GameName = gameName;
             this.QuestCompletedPopupText = questCompletedPopupText;
             this.Tags = tags;
+            this.ControlTags = controlTags;
             this.Categories = categories;
             this.OnboardingExperience = onboardingExperience;
         }
@@ -185,6 +187,13 @@ namespace Immutable.Api.ZkEvm.Model
         public List<string> Tags { get; set; }
 
         /// <summary>
+        /// Some specific tags for controlling feed items
+        /// </summary>
+        /// <value>Some specific tags for controlling feed items</value>
+        [DataMember(Name = "control_tags", EmitDefaultValue = false)]
+        public List<string> ControlTags { get; set; }
+
+        /// <summary>
         /// The categories for the feed item
         /// </summary>
         /// <value>The categories for the feed item</value>
@@ -217,6 +226,7 @@ namespace Immutable.Api.ZkEvm.Model
             sb.Append("  GameName: ").Append(GameName).Append("\n");
             sb.Append("  QuestCompletedPopupText: ").Append(QuestCompletedPopupText).Append("\n");
             sb.Append("  Tags: ").Append(Tags).Append("\n");
+            sb.Append("  ControlTags: ").Append(ControlTags).Append("\n");
             sb.Append("  Categories: ").Append(Categories).Append("\n");
             sb.Append("  OnboardingExperience: ").Append(OnboardingExperience).Append("\n");
             sb.Append("  Type: ").Append(Type).Append("\n");
