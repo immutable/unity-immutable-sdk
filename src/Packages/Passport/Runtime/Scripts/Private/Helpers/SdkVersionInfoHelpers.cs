280f2a90f58064a207462a5c044db2028dc24ef1
Create or Update Pull Request Action <create-or-update-pull-request@users.noreply.github.com>
2025-12-22 00:30:10 +0000

    chore: update version

--- src/Packages/Passport/Runtime/Scripts/Private/Helpers/SdkVersionInfoHelpers.cs
+++ src/Packages/Passport/Runtime/Scripts/Private/Helpers/SdkVersionInfoHelpers.cs
@@ -4,7 +4,7 @@ namespace Immutable.Passport.Helpers
     {
         public static string GetSdkVersionInfo()
         {
-            return "1.36.9";
+            return "1.36.10";
         }
     }
 }
